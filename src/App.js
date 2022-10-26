ea9a3110110b1f33573594e1cd9dc52b1e02fa18
Pablo Carballo <pscarballo@gmail.com>
2022-10-25 23:30:52 -0300

    agrego Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 import './App.css';
+import ItemListContainer from './components/ItemListContainer';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      hola mundo
-    </div>
+    <>
+      <div>
+        <Navbar />
+        <ItemListContainer />
+
+
+      </div>
+
+    </>
   );
 }
 
